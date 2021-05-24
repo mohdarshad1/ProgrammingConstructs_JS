e37be83dfb4070f4099dd310eb78ca813792c786
mohdarshad1 <mdarshad181@gmail.com>
2021-05-24 18:18:44 +0530

    [Mohd Arshad] : if _else _if&else 1

--- if _else _if&else 1.js
+++ if _else _if&else 1.js
@@ -1,16 +1,22 @@
-let day = Math.floor(Math.random()*10)%7+1;
-console.log(day)
-if(day==1)
-    console.log("Monday");
-else if(day==2)
-    console.log("Tuesday");
-else if(day==3)
-    console.log("Wednesday");
-else if(day==4)
-    console.log("Thursday");
-else if(day==5)
-    console.log("Friday");
-else if(day==6)
-    console.log("Saturday");
-else if(day==7)
-    console.log("Sunday");
+var num = Math.floor(Math.random()*10);
+console.log(num);
+if(num==1)
+    console.log("One");
+else if(num==2)
+    console.log("Two");
+else if(num==3)
+    console.log("Three");
+else if(num==4)
+    console.log("Four");
+else if(num==5)
+    console.log("Five");
+else if(num==6)
+    console.log("Six");
+else if(num==7)
+    console.log("Seven");
+else if(num==8)
+    console.log("Eight");
+else if(num==9)
+    console.log("Nine");
+else if(num==0)
+    console.log("Zero");   
